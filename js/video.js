cba9893ab4ec2fce0acdfb0b77cdb4cf50f866cc
Victor Bailiot <victor_bailiot@hotmail.com>
2022-07-11 12:40:26 -0400

    Update depoimentos

---
+++ js/video.js
@@ -0,0 +1,33 @@
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// Mudar video aqui
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('video1', {
+        height: '380',
+        width: '580',
+        videoId: 'xhhGm2hEDyc',
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+
+}
+
+function onPlayerReady(event) {
+    event.target.stopVideo();
+}
+
+var done = false;
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+        done = true;
+    }
+}
+function stopVideo() {
+    player.stopVideo();
+}
